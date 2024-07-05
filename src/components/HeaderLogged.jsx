b0ec61b6203780918145d53e5a0e865275ff5e8d
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-07-05 20:17:32 -0300

    header logged

--- src/components/HeaderLogged.jsx
+++ src/components/HeaderLogged.jsx
@@ -1,25 +1,31 @@
 import { useNavigate } from "react-router-dom"
 import './css/Header.css'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import socket from "../utils/socket.js"
 import { useUser } from "./UserProvider.jsx"
 
 export const HeaderLogged = () => {
     const navigate = useNavigate()
     const { user } = useUser()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        
         //TODO
-        socket.on('updateHeader', () => {
-            
-        })
-
-        return () => {
-            socket.off('updateHeader')
+        // socket.on('updateHeader', () => {
+        if (user) {
+            setLoading(false)
         }
+        // })
 
-    }, [])
+        // return () => {
+        //     socket.off('updateHeader')
+        // }
+
+    }, [user])
+
+    if (loading || !user) {
+        return <div></div>;
+    }
     
     return(
         <>
