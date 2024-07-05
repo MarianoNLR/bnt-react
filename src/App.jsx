66bbde1de6749f913e94ddb4f29acd2589917892
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-07-05 05:18:10 -0300

    app settings

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { Home } from './pages/Home.jsx'
+import { LoginPage } from './pages/LoginPage.jsx'
+import { RegisterPage } from './pages/RegisterPage.jsx'
+import { GameBet } from './pages/GameBet.jsx'
+import { MyGames } from './pages/MyGames.jsx'
+import { LoggedLayout } from './components/LoggedLayout.jsx'
+import { AuthLayout } from './components/AuthLayout.jsx'
+import { GameView } from './pages/GameView.jsx'
+import './App.css'
+import { useUser } from './components/UserProvider.jsx'
+
+function App() {
+  const user = useUser()
+  return (
+    <>
+        <Routes>
+          <Route element={<LoggedLayout />}>
+            <Route index path='/' element={ user ? <Home /> : <Navigate to="/login" />}></Route>
+            <Route path='/room/:room' element={ user ? <GameBet /> : <Navigate to="/login" />}></Route>
+            <Route path='/my_games' element={<MyGames />}></Route>
+            <Route path='/game_view/:room' element={<GameView />}></Route>
+          </Route>
+
+          <Route element={<AuthLayout />}>
+            <Route path='/login' element={<LoginPage />}></Route>
+            <Route path='/register' element={<RegisterPage />}></Route>
+          </Route>
+        </Routes>
+    </>
+  )
+}
+
+export default App
