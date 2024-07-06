f5e64857ff42066691da5e474a834397df09e03c
Mariano Lotero Rolon <78249505+MarianoNLR@users.noreply.github.com>
2024-07-06 02:29:26 -0300

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = 'https://bnt-app.onrender.com/'
+const SOCKET_URL = 'https://bnt-app.vercel.app/'
 
 const socket = io(SOCKET_URL, {
     withCredentials: true
