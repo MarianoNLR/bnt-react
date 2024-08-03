26078958001ca1b807c3ce19b9637164b9749390
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-03 00:10:49 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: parseInt(import.meta.env.VITE_PORT, 10) || 5173,
+    host: true,
   },
   preview: {
-    port: 4173
+    port: parseInt(import.meta.env.VITE_PORT, 10) || 4173,
+    host: true,
   }
 })
